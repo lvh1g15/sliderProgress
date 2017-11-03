91b30bfdf5bd9c86b304e719212d165fe593b2af
Landon Vago-Hughes <landon2008@hotmail.com>
2017-11-03 14:14:17 +0000

    0.2.0

--- sliderdismissanimation/Classes/SwipeProgression.swift
+++ sliderdismissanimation/Classes/SwipeProgression.swift
@@ -42,4 +42,28 @@ public class SwipeProgression: UIView {
             }
         })
     }
+    
+    public func swipeDownProgress(scrollviewcurrent: UIScrollView) {
+        
+        let superviewheight = superview?.layer.frame.height
+        
+        let superviewwidth = superview?.layer.frame.width
+        
+        let verticaloffset = scrollviewcurrent.contentOffset.y
+        
+        let totalscrollheight = scrollviewcurrent.contentSize.height - superviewheight!
+        
+        let ratio = (verticaloffset/totalscrollheight) * superviewwidth!
+        
+        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseOut, animations: {
+            self.snp.updateConstraints({(makes) -> Void in
+                makes.width.equalTo(ratio)
+            })
+        }, completion: { stop in
+            if ratio > 1.05*superviewwidth! {
+                self.removeFromSuperview()
+                return
+            }
+        })
+    }
 }
